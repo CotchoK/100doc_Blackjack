071877192dcbfe71d3341ecd20f8c21793457d60
Con <ckritikos@gmail.com>
2024-04-24 00:42:49 +1000

    Add comments to main.py and actions.py Add ascii_art file for logo Updated logic to allow the player to play again

--- actions.py
+++ actions.py
@@ -2,6 +2,13 @@ import random as r
 
 # deal mechanism
 def deal(player_h, dealer_h, deck):
+    """
+    Function that deals cards to the player and the dealer
+    :param player_h: the players hand (list object)
+    :param dealer_h: the dealers hand (list object)
+    :param deck: the deck of cards we have (list object)
+    :return: returns the player and dealer hands with two cards for each
+    """
     for i in range(0, 2):
         player_h.append(r.choice(deck))
         dealer_h.append(r.choice(deck))
@@ -9,12 +16,23 @@ def deal(player_h, dealer_h, deck):
 
 
 def hit(hand, deck):
+    """
+    Function that will add another card to the persons (player or dealer) hand
+    :param hand: the player or the dealer as passed
+    :param deck: the deck of cards (list object)
+    :return: the updated hand of cards
+    """
     hand.append(r.choice(deck))
     # hand.append(11) #test line
     return hand
 
 
 def check_hand(hand):
+    """
+    Checks to see the value of the hand
+    :param hand: the hand we want to check the value of
+    :return: the score/value of the hand
+    """
     total = 0
     for i in hand:
         total += i
--- main.py
+++ main.py
@@ -4,8 +4,7 @@
 # there are no jokers
 # Jack/Queen/King all valued at 10
 # ace can count as 11 or 1
-## using the following list of cards
-cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
 # the cards in the list have equal probability of being drawn
 # cards are not removed from the deck as they are drawn.
 
@@ -18,8 +17,17 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 # if over 21 then lose
 # if under 21 and stand, dealer deal
 
+########################################################
+
 #import scripts
 import actions as act
+import ascii_art as aa
+
+## using the following list of cards
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+
+# print logo
+print(aa.logo)
 
 # define player and dealer hands
 player_hand = []
@@ -29,79 +37,96 @@ dealer_hand = []
 # player_hand = [10, 10]
 # dealer_hand = [11, 11]
 
+# gameplay handling variables
 round_end = False
+game_end = False
 round_start = True
 player_stand = False
 
-while not round_end:
-    if round_start:
-        #act.deal(player_hand, dealer_hand, cards)
-        round_start = False
+# check to see if game is ended
+while not game_end:
+    # while the round has not ended execute the game loop
+    while not round_end:
+        # if it is the start of the game then let's deal cards and update round_start flag to false
+        if round_start:
+            act.deal(player_hand, dealer_hand, cards)
+            round_start = False
 
-    player_hand_value = act.check_hand(player_hand)
-    dealer_hand_value = act.check_hand(dealer_hand)
+        # calculate value of both player and dealer hand
+        player_hand_value = act.check_hand(player_hand)
+        dealer_hand_value = act.check_hand(dealer_hand)
 
-    print(f"player hand: {player_hand} | value: {player_hand_value}")
-    print(f"dealer hand: {dealer_hand} | value: {dealer_hand_value}")
+        # print out the player hand and value and the dealer partial hand
+        print(f"player hand: {player_hand} | value: {player_hand_value}")
+        print(f"dealer hand: [#, {', '.join(map(str, dealer_hand[1:]))}]")
 
-    if not player_stand:
-        if player_hand_value == 21:
-            player_stand = True
-        elif player_hand_value < 21:
-            response = input("Would you like to 'hit' or 'stand'? ") # ask to hit or stand
-            if response == 'hit':
-                act.hit(player_hand, cards)
-            elif response == 'stand':
+        # check if the player stands
+        if not player_stand:
+            if player_hand_value == 21:
                 player_stand = True
-        elif player_hand_value > 21:
-            if 11 in player_hand:
-                player_hand[player_hand.index(11)] = 1
-                print("you had an 11")
+            elif player_hand_value < 21:
+                response = input("Would you like to 'hit' or 'stand'? ") # ask to hit or stand
+                if response == 'hit':
+                    act.hit(player_hand, cards)
+                elif response == 'stand':
+                    player_stand = True
+            elif player_hand_value > 21:
+                if 11 in player_hand:
+                    player_hand[player_hand.index(11)] = 1
+                    print("you had an 11")
+                else:
+                    print("You bust.")
+                    round_end = True
+
+        # if the player stands, then execute dealer based logic and win/lose conditions
+        if player_stand:
+            if dealer_hand_value > 21:
+                if 11 in dealer_hand:
+                    dealer_hand[dealer_hand.index(11)] = 1
+                    print("dealer had an 11")
+                else:
+                    print("Dealer bust")
+                    round_end = True
+            elif dealer_hand_value < 17:
+                act.hit(dealer_hand, cards)
+            elif dealer_hand_value == player_hand_value:
+                print("It's a draw")
+                round_end = True
+            elif dealer_hand_value > player_hand_value:
+                if dealer_hand_value == 21 and len(dealer_hand) == 2:
+                    print("Dealer wins. Dealer got blackjack")
+                else:
+                    print("Dealer wins")
+                round_end = True
             else:
-                print("You bust.")
+                if player_hand_value == 21 and len(player_hand) == 2:
+                    print("You win. You got blackjack")
+                else:
+                    print("You win")
                 round_end = True
 
-    if player_stand:
-        if dealer_hand_value > 21:
-            if 11 in dealer_hand:
-                dealer_hand[dealer_hand.index(11)] = 1
-                print("dealer had an 11")
+        if round_end:
+            # for testing purposes
+            print(f"\ndealer hand: {dealer_hand} | value: {dealer_hand_value}")
+
+            # check if the player wants to play again
+            game_response = input("Would you like to play again? ('y' for yes, 'n' for no): ")
+            # if the player doesn't want to play again exit the program.
+            if game_response == 'n':
+                exit()
+            # if the player wants to play again reset all game objects to default
             else:
-                print("Dealer bust")
-                round_end = True
-        elif dealer_hand_value < 17:
-            act.hit(dealer_hand, cards)
-        elif dealer_hand_value == player_hand_value:
-            print("It's a draw")
-            round_end = True
-        elif dealer_hand_value > player_hand_value:
-            if dealer_hand_value == 21 and len(dealer_hand) == 2:
-                print("Dealer wins. Dealer got blackjack")
-            else:
-                print("Dealer wins")
-            round_end = True
-        else:
-            if player_hand_value == 21 and len(player_hand) == 2:
-                print("You win. You got blackjack")
-            else:
-                print("You win")
-            round_end = True
+                round_start = True
+                round_end = False
+                player_stand = False
+                player_hand.clear()
+                dealer_hand.clear()
+                print("\n")
+
+# troubleshooting
 
 
 
-
-
-#player_hand, dealer_hand = deal(player_hand, dealer_hand, cards)
-
-
-# print(player_hand)
-# print(dealer_hand)
-#
-# hit(player_hand, cards)
-# hit(dealer_hand, cards)
-#
-# print(player_hand)
-# print(dealer_hand)
 
 
 
